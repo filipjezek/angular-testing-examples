42f5101efec01ce89634d8db97c82d81b6f053fa
Filip Ježek <filip.jezek@alza.cz>
2018-06-11 16:32:27 +0200

    correctly typed jasmine-marbles-examples

--- src/app/examples/jasmine-marbles-examples.ts
+++ src/app/examples/jasmine-marbles-examples.ts
@@ -1,11 +1,10 @@
 import { hot, cold } from 'jasmine-marbles';
 import { merge } from 'rxjs/operators'
 import { Observable } from 'rxjs/Observable';
-import { TestColdObservable } from 'jasmine-marbles/src/test-observables';
-import { TestScheduler } from 'rxjs/testing/TestScheduler';
+import { TestColdObservable, TestHotObservable } from 'jasmine-marbles/src/test-observables';
 
-var e1: Observable<any> = hot('----a--^--b-------c--|');
-var e2 = hot(  '---d-^--e---------f-----|');
+var e1: TestHotObservable = hot('----a--^--b-------c--|');
+var e2: TestHotObservable = hot(  '---d-^--e---------f-----|');
 var expected: TestColdObservable =      cold('---(be)----c-f-----|');
 
 expect(merge(e1, e2)).toBeObservable(expected);
