082ca40fc64e41e7c7555b62d9e9fef3ec15359f
Filip Ježek <filip.jezek@alza.cz>
2018-06-11 16:35:00 +0200

    indented

--- src/app/examples/jasmine-marbles-examples.ts
+++ src/app/examples/jasmine-marbles-examples.ts
@@ -3,8 +3,8 @@ import { merge } from 'rxjs/operators'
 import { Observable } from 'rxjs/Observable';
 import { TestColdObservable, TestHotObservable } from 'jasmine-marbles/src/test-observables';
 
-var e1: TestHotObservable = hot('----a--^--b-------c--|');
-var e2: TestHotObservable = hot(  '---d-^--e---------f-----|');
-var expected: TestColdObservable =      cold('---(be)----c-f-----|');
+var e1: TestHotObservable =  hot('----a--^--b-------c--|');
+var e2: TestHotObservable =  hot(  '---d-^--e---------f-----|');
+var expected: TestColdObservable = cold('---(be)----c-f-----|');
 
 expect(merge(e1, e2)).toBeObservable(expected);
